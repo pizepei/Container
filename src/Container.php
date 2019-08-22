d8047d0d369c25a29695412fde33cf691a8add86
pizepei <848733913@qq.com>
2019-08-22 22:36:25 +0800

    完善容器

--- src/Container.php
+++ src/Container.php
@@ -21,12 +21,12 @@ class Container implements ContainerInterface
      * @var array
      */
     protected $instances = [];
-
     /**
-     * 基础容器中的对象实例
+     * 函数
      * @var array
      */
-    protected $baseInstances = [];
+    protected $closure  = [];
+
     /**
      * 容器绑定标识
      * @var array
@@ -45,38 +45,69 @@ class Container implements ContainerInterface
     protected $invokeCallback = [];
 
     /**
-     * 注册一个服务
+     * 注册一个普通服务
      */
-    protected function setInstances($name ,$instances)
+    protected function setInstances($name ,$instances,bool $new=false)
     {
-        if (isset($instances[$name])){
-            
+        # 不能绑定注册基础服务标识
+        if (isset($this->baseBind[$name])){
+            return false;
+        }
+
+        if ($concrete instanceof Closure) {# 如果是函数，先注册到bind中
+            # 如果是true 就无论如何的写入bind
+            if ($new){
+                # 强制更新 这里如果原来有 不管是函数函数类的实例都会被覆盖
+                $this->bind[$name] = $instances;
+                return true;
+            }else{
+
+                # 判断是否已经有有就返回false 注册失败    这里同时判断是否已经有绑定的服务  （是否注册由isset($this->instances[$name])决定，因为所有类型服务都放在这里）
+                if (isset($this->bind[$name]) || isset($this->instances[$name])){
+                    return false;
+                }else{
+                    $this->bind[$name] = $instances;
+                    return true;
+                }
+
+            }
+        } else {
+            # 如果是一个实例化的对象就直接绑定到服务中
+            if ($new) {
+                # 强制更新 这里如果原来有 不管是函数函数类的实例都会被覆盖
+                $this->instances[$abstract] = $concrete;
+            }else{
+                # 判断是否已经有有就返回false 注册失败
+                if (isset($this->instances[$name])){
+                    return false;
+                }else{
+                    $this->instances[$abstract] = $concrete;
+                    return true;
+                }
+            }
         }
 
     }
-
-
     /**
-     * 当调用一个不存在的方法时使用
+     * 当调用一个不存在的方法时使用（以动态方法方式使用服务）
      * @param $name
      * @param $arguments
      * @return bool|mixed
      */
     public function __call($name, $arguments)
     {
-        /**
-         * 判断是否在基础中
-         */
-        if ($this->baseBind[$name]){
-            if (!isset($this->baseInstances[$name])){
-
+        # 判断是否在绑定中
+        if (isset($this->baseBind[$name]) || isset($this->bind[$name])){
+            # 判断是否已经注册
+            if (!isset($this->instances[$name])){
+                # 判断是否有参数
                 if (!empty($arguments)){
-                    $this->baseInstances[$name] = new  $this->baseBind[$name](...$arguments);
+                    $this->instances[$name] = new  $this->baseBind[$name](...$arguments);
                 }else{
-                    $this->baseInstances[$name] = new  $this->baseBind[$name]();
+                    $this->instances[$name] = new  $this->baseBind[$name]();
                 }
             }
-            return $this->baseInstances[$name];
+            return $this->instances[$name];
         }
         return false;
     }
@@ -114,14 +145,12 @@ class Container implements ContainerInterface
      *
      * @return bool
      */
-    public function has($id)
+    public function has(string $id):bool
     {
-        if (isset($this->baseInstances[$id])){
-            return true;
-        }
         if (isset($this->instances[$id])){
             return true;
         }
         return false;
     }
+
 }
